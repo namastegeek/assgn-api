6a6d4afdf4b859edfb24795f3ca0e5145c5414d8
“NamasteGeek ”git config --global user.email “techchic90@gmail.com ”git config --global user.name “NamasteGeek <40542070+namastegeek@users.noreply.github.com>
2021-04-25 12:42:07 -0500

    rename router file,  connecting Express Db

--- app.js
+++ app.js
@@ -4,3 +4,33 @@
 //use body-parser to handle requests sent
 
 
+let express = require('express')
+let app = express();
+const apiRouter = require("./assgn-router.js");
+let mongoose = require("mongoose");
+let bodyParser = require("body-parser");
+
+// set up mongoose and body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+// connect to mongoose
+mongoose.connect("mongodb://localhost/assgn", { useNewUrlParser: true});
+var db = mongoose.connection;
+
+if(!db) {
+    console.log("Error connecting to the DB.");
+} else {
+    console.log("DB connected successfully");
+}
+
+var port = 8080;
+
+app.use("/api", apiRouter);
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.listen(port, function () {
+    console.log("Running assgn-api on port " + port);
+});
